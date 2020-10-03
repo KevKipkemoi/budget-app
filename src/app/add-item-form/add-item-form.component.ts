d7a1ddd0ad53925eb3cad10262e30bd53dfa636c
KevKipkemoi <kevkipkemoi@outlook.com>
2020-10-03 14:37:31 +0300

    UI logic

--- src/app/add-item-form/add-item-form.component.ts
+++ src/app/add-item-form/add-item-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { BudgetItem } from 'src/shared/models/budget-item-model';
 
 @Component({
   selector: 'app-add-item-form',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddItemFormComponent implements OnInit {
 
+  @Input() item: BudgetItem;
+  @Output() formSubmit: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
+
+  isNewItem: boolean;
+
   constructor() { }
 
   ngOnInit(): void {
+    if (this.item) {
+      this.isNewItem = false;
+    } else {
+      this.isNewItem = true;
+      this.item = new BudgetItem('', null);
+    }
+  }
+
+  onSubmit(form: NgForm) {
+    this.formSubmit.emit(form.value);
+    form.reset();
   }
 
 }
